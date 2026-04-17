fe89be1ecd06351739febf22fdf8ec957057d8fb
AleDev90 <alegarsandev@gmail.com>
2026-04-17 13:12:43 +0200

    nuevo boton y prueba de if

--- lib/main.dart
+++ lib/main.dart
@@ -76,22 +76,34 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
+      
+      
+      if(_counter<= 9){
       _counter++;
+      }
+
+
 
       
 
       
       
     });
+  }
 
-    
-
-    
+  void _decrementcounter(){
+    setState((){
+      if(_counter>=0){
+      _counter--;}
+    });
   }
 
   String mensaje(){
-    if (_counter>10){ 
-      return "has pulsado demasadas veces el boton";}
+    if (_counter>=10){ 
+      return "has pulsado demasadas veces el boton";}else if(_counter<0 ){
+        _counter++;
+        return "No puedes elegir numero negativo";
+      }
       return "puedes seguir";
   }
 
@@ -137,7 +149,10 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text('You have pushed the button this many times:'),
-            Text(mensaje()),
+            Text(mensaje(),
+            style: Theme.of(context).textTheme.headlineLarge?.copyWith(color: Colors.red),
+             
+            ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
@@ -146,11 +161,27 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
+      
+      
+      
+      
+      floatingActionButton: Column( mainAxisAlignment: MainAxisAlignment.center,
+      children:[
+        FloatingActionButton(
         onPressed: _incrementCounter, 
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
+      const SizedBox(width:10,height: 10,),
+      FloatingActionButton(
+        onPressed: _decrementcounter,
+        tooltip: "decrement",
+        child: const Icon(Icons.remove),
+        )
+      ]
+      )
+
+      
     );
   }
 }
